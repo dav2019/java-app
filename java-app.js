ae8326bb5b5a8ccf6bd1fffad2c53019d01a141a
David Bui <davidb9@outlook.com.au>
2019-06-05 17:09:40 +1000

    added some style and a bit of javascript

---
+++ java-app.js
@@ -0,0 +1,3 @@
+document.querySelector(".zero").innerHTML = `<p>${0}</p>`;
+document.querySelector(".decimal").innerHTML = `<p>${'.'}</p>`;
+document.querySelector(".equal").innerHTML = `<p>${'='}</p>`;
